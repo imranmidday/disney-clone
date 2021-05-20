eae7c581612b7ba84e6a5178cb2fe3fd5a3e0c4f
Imran Midday <imranmidday6666@gmail.com>
2021-05-20 20:47:37 +0530

    ADD detail page from db

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import styled from 'styled-components'
+import { Link } from 'react-router-dom'
 import { selectMovies } from '../features/movie/movieSlice'
 import { useSelector } from 'react-redux'
 function Movies() {
 
     const movies  = useSelector(selectMovies);
-    console.log("This is movies", movies); 
 
 
     return (
@@ -15,7 +15,9 @@ function Movies() {
                 { movies &&
                     movies.map((movie)=>(
                         <Wrap key={movie.id}>
-                            <img src={ movie.cardImg }/>
+                            <Link to={`/detail/${movie.id}`}>
+                                <img src={ movie.cardImg }/>
+                            </Link>
                         </Wrap>
                     ))
 
@@ -28,7 +30,7 @@ function Movies() {
 export default Movies
 
 const Container = styled.div`
-    overflow: hidden;
+    
 `
 
 const Content = styled.div`
